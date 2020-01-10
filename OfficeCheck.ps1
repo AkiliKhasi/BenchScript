f3e8ddc84bbf9a68e7a383def6adfc9c8e5b71b2
Kienan Hicks <kienan.hicks@gmail.com>
2020-01-10 14:05:46 -0500

    Reorganized files

--- OfficeCheck.ps1
+++
@@ -1,3 +0,0 @@
-$AppList = get-package -name "*microsoft office*"
-$array = ($AppList | Select-Object Name)
-if ($array -match 'Microsoft Office*'){.\o15-ctrremove.diagcab}else {'Office is not installed'}
--- WindowsUpdate.ps1
+++
@@ -1,6 +0,0 @@
-Write-Host "Configuring Windows Update Settings"
-cd c:
-Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings -Name BranchReadinessLevel -Value 32
-Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings -Name DeferFeatureUpdatesPeriodInDays -Value 90
-Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Services\7971f918-a847-4430-9279-4a52d1efe18d -Name RegisteredWithAU -Value 1
-Write-Host "Windows Update Configured"
