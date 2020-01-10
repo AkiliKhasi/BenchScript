7bfba85de03bcd6269f61913d0c18dad3339869e
Kienan Hicks <kienan.hicks@gmail.com>
2020-01-10 13:39:37 -0500

    Initial commit

---
+++ BenchScript.ps1
@@ -0,0 +1,116 @@
+do {
+  $Input = 0
+  while ( $Input -lt 1 -or $Input -gt 5) {
+    Write-Host "1. Complete Bench Cycle"
+    Write-Host "2. Remove Bloatware"
+    Write-Host "3. Set Default Settings"
+	Write-Host "4. Install Java"
+    Write-Host "5. Install Adobe Reader"
+
+    $Input = Read-Host "Please choose an option" -ErrorAction Stop
+
+    switch ($Input) {
+      1{.\hpdesktop.ps1}
+      2{Write-Host "Uninstalling Bloatware"
+			get-package -name "hp device*" | uninstall-package -force
+			get-package -name "hp support as*" | uninstall-package -force
+			get-package -name "hp*" | uninstall-package -force
+			Get-AppxPackage -AllUsers | where-object {($_.name -notlike "*store*") -and ($_.name -notlike "*calc*") -and ($_.name -notlike "*stickynotes*")} | Remove-AppxPackage
+			Get-appxprovisionedpackage -online | where-object {($_.packagename -notlike "*store*") -and ($_.packagename -notlike "*calc*") -and ($_.packagename -notlike "*stickynotes*")} | Remove-AppxProvisionedPackage -online }
+			
+      3{import-module .\switchuaclevel.psm1
+			Write-Host "UAC Level"
+				set-uaclevel 0
+			Write-Host "Enabling Remote Desktop"
+			Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 0
+			Write-Host "Complete"
+			Write-Host "Power Setting Configuration"
+				powercfg -change -monitor-timeout-ac 0
+				powercfg -change -monitor-timeout-dc 0
+				powercfg -change -standby-timeout-ac 0
+				powercfg -change -standby-timeout-dc 0
+			Write-Host "Power Settings Configured"
+			Write-Host "Disabling Firewall"
+				Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+			Write-Host "Firewall Disabled"
+				Get-AppxPackage *officehub* | Remove-AppxPackage
+			.\o15-ctrremove.diagcab
+			Write-Host "Configuring Windows Update Settings"
+				cd c:
+				Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings -Name BranchReadinessLevel -Value 32
+				Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings -Name DeferFeatureUpdatesPeriodInDays -Value 90
+				Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Services\7971f918-a847-4430-9279-4a52d1efe18d -Name RegisteredWithAU -Value 1
+				Set-ItemProperty -Path 'Registry::HKU\.DEFAULT\Control Panel\Keyboard' -Name "InitialKeyboardIndicators" -Value "2"
+			Write-Host "Windows Update Configured"} 
+	  4{cd c:
+			Write-Host "Installing Java"
+				New-Item -path "c:\" -Name "Source" -ItemType "directory"
+				$url = "http://javadl.oracle.com/webapps/download/AutoDL?BundleId=234474_96a7b8442fe848ef90c96a2fad6ed6d1"
+				$output = "c:\source\javainstall.exe"
+				Invoke-WebRequest -uri $url -OutFile $output
+				cd c:\source
+				.\javainstall.exe}
+	  5{cd c:
+			Function Get-FTPFileList { 
+			
+			Param (
+			[System.Uri]$server,
+			[string]$directory
+			)
+
+			try 
+			{
+			$uri =  "ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/" 
+
+			$FTPRequest = [System.Net.FtpWebRequest]::Create($uri)
+			
+			$FTPRequest.Method = [System.Net.WebRequestMethods+Ftp]::ListDirectoryDetails
+    
+			$FTPResponse = $FTPRequest.GetResponse() 
+    
+			$ResponseStream = $FTPResponse.GetResponseStream()
+    
+			$StreamReader = New-Object System.IO.StreamReader $ResponseStream  
+   
+			$files = New-Object System.Collections.ArrayList
+			While ($file = $StreamReader.ReadLine())
+				{
+				[void] $files.add("$file")
+      
+				}
+
+			}
+			catch {
+				write-host -message $_.Exception.InnerException.Message
+				}
+
+			$StreamReader.close()
+			$ResponseStream.close()
+			$FTPResponse.Close()
+
+			Return $files
+
+			}
+
+			$server = 'ftp://ftp.adobe.com/'
+			$directory ='pub/adobe/reader/win/AcrobatDC/'
+
+			$filelist = Get-FTPFileList -server $server -directory $directory
+
+			$file = $filelist | Select -First 1
+
+			$filename = $file.substring($file.get_length()-10)
+
+			$FTPFolderUrl = $server + $directory
+
+			$LatestFile = "AcroRdrDC" + $filename + "_en_US.exe"
+			$DownloadURL = $FTPFolderUrl + $filename + "/" + $latestfile
+			$output = "c:\source\AdobeInstall.exe"
+			Invoke-WebRequest -uri $DownloadURL -OutFile $output
+			cd c:\source
+			.\AdobeInstall.exe}
+
+	default {Write-Host "Nothing selected"}
+    }
+  }
+} while ( $Input -ne 5 )
---
+++ Update.ps1
@@ -0,0 +1,133 @@
+Function Get-LatestUpdate {
+    <#
+    .SYNOPSIS
+        Get the latest Cumulative update for Windows
+
+    .DESCRIPTION
+        This script will return the list of Cumulative updates for Windows 10 and Windows Server 2016 from the Microsoft Update Catalog. Optionally download the updates using the -Download parameter.
+
+    .NOTES
+        Name: Get-LatestUpdate
+        Author: Aaron Parker
+        Twitter: @stealthpuppy
+
+        Original script: Copyright Keith Garner, All rights reserved.
+        Forked from: https://gist.github.com/keithga/1ad0abd1f7ba6e2f8aff63d94ab03048
+
+    .LINK
+        https://support.microsoft.com/en-us/help/4043454
+
+    .EXAMPLE
+        Get-LatestUpdate
+
+        Description:
+        Get the latest Cumulative Update for Windows 10 x64
+
+    .PARAMETER SearchString
+        Specify a specific search string to change the target update behaviour. The default will only download Cumulative updates for x64.
+
+    .EXAMPLE
+        Get-LatestUpdate -SearchString 'Cumulative.*x86'
+
+        Description:
+        Enumerate the latest Cumulative Update for Windows 10 x86 (Semi-Annual Channel)
+
+    .EXAMPLE
+        Get-LatestUpdate.ps1 -SearchString 'Cumulative.*Server.*x64' -Build 14393 
+    
+        Description:
+        Enumerate the latest Cumulative Update for Windows Server 2016
+    #>
+    [CmdletBinding(SupportsShouldProcess = $False)]
+    Param(
+        [Parameter(Mandatory = $False, HelpMessage = "JSON source for the update KB articles.")]
+        [Parameter(ParameterSetName = 'Download', Mandatory = $False)]
+        [string]$StartKB = 'https://support.microsoft.com/app/content/api/content/asset/en-us/4000816',
+
+        [Parameter(Mandatory = $False, HelpMessage = "Windows build number.")]
+        [ValidateSet('16299', '15063', '14393', '10586', '10240')]
+        [string]$Build = '16299',
+
+        [Parameter(Mandatory = $False, HelpMessage = "Search query string.")]
+        [string]$SearchString = 'Cumulative.*x64'
+    )
+    Begin {
+    }
+    Process {
+        #region Find the KB Article Number
+        Write-Verbose "Downloading $StartKB to retrieve the list of updates."
+        $kbID = (Invoke-WebRequest -Uri $StartKB).Content |
+            ConvertFrom-Json |
+            Select-Object -ExpandProperty Links |
+            Where-Object level -eq 2 |
+            Where-Object text -match $Build |
+            Select-LatestUpdate |
+            Select-Object -First 1
+        #endregion
+
+        #region get the download link from Windows Update
+        $Kb = $kbID.articleID
+        Write-Verbose "Found ID: KB$($kbID.articleID)"
+        $kbObj = Invoke-WebRequest -Uri "http://www.catalog.update.microsoft.com/Search.aspx?q=KB$($kbID.articleID)"
+
+        $Available_kbIDs = $kbObj.InputFields | 
+            Where-Object { $_.Type -eq 'Button' -and $_.Value -eq 'Download' } | 
+            Select-Object -ExpandProperty ID
+
+        $Available_kbIDs | Out-String | Write-Verbose
+
+        $kbIDs = $kbObj.Links | 
+            Where-Object ID -match '_link' |
+            Where-Object innerText -match $SearchString |
+            ForEach-Object { $_.Id.Replace('_link', '') } |
+            Where-Object { $_ -in $Available_kbIDs }
+
+        # If innerHTML is empty or does not exist, use outerHTML instead
+        If ( $Null -eq $kbIDs ) {
+            $kbIDs = $kbObj.Links | 
+                Where-Object ID -match '_link' |
+                Where-Object outerHTML -match $SearchString |
+                ForEach-Object { $_.Id.Replace('_link', '') } |
+                Where-Object { $_ -in $Available_kbIDs }
+        }
+
+        $Urls = @()
+        ForEach ( $kbID in $kbIDs ) {
+            Write-Verbose "`t`tDownload $kbID"
+            $Post = @{ size = 0; updateID = $kbID; uidInfo = $kbID } | ConvertTo-Json -Compress
+            $PostBody = @{ updateIDs = "[$Post]" } 
+            $Urls += Invoke-WebRequest -Uri 'http://www.catalog.update.microsoft.com/DownloadDialog.aspx' -Method Post -Body $postBody |
+                Select-Object -ExpandProperty Content |
+                Select-String -AllMatches -Pattern "(http[s]?\://download\.windowsupdate\.com\/[^\'\""]*)" | 
+                ForEach-Object { $_.matches.value }
+        }
+        #endregion 
+    }
+    End {
+        $Notes = $kbObj.ParsedHtml.body.getElementsByTagName('a') | ForEach-Object InnerText | Where-Object { $_ -match $SearchString }
+        [int]$i = 0; $Output = @()
+        ForEach ( $Url in $Urls ) {
+            $item = New-Object PSObject
+            $item | Add-Member -type NoteProperty -Name 'KB' -Value "KB$Kb"
+            If ( $Notes.Count -eq 1 ) {
+                $item | Add-Member -type NoteProperty -Name 'Note' -Value $Notes
+            }
+            Else {
+                $item | Add-Member -type NoteProperty -Name 'Note' -Value $Notes[$i]
+            }
+            $item | Add-Member -type NoteProperty -Name 'URL' -Value $Url
+            If ($PSBoundParameters.ContainsKey('Download')) {
+                $item | Add-Member -type NoteProperty -Name 'File' -Value $Url.Substring($Url.LastIndexOf("/") + 1)
+            }
+            If ($PSBoundParameters.ContainsKey('Path')) {
+                $item | Add-Member -type NoteProperty -Name 'UpdatePath' -Value "$((Get-Item $Path).FullName)"
+            }
+
+            $Output += $item
+            $i = $i + 1
+        }
+
+        # Write the URLs list to the pipeline
+        Write-Output $Output
+    }
+}
